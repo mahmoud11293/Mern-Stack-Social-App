0cae0bde5342a91ee2ceda91b1c021eccf2584ae
mahmoud <m.hamed11293@gmail.com>
2025-12-17 09:42:19 +0200

    add new file

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './User/user.module';
-import { PostModule } from './post/post.module';
+import { PostModule } from './Post/post.module';
 import { CommentModule } from './comment/comment.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
